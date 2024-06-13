405ad854f2cb197639810459d720c4e26261f347
Abhishek Aman <abhiofficial.1599@gmail.com>
2024-06-13 19:16:44 +0530

    Icon and Button Config

--- force-app/main/default/lwc/introduction/introduction.js
+++ force-app/main/default/lwc/introduction/introduction.js
@@ -1,12 +1,26 @@
 import { LightningElement } from 'lwc';
 import Images from '@salesforce/resourceUrl/Images';
+import RESUME from '@salesforce/resourceUrl/Document';
+import { loadScript } from 'lightning/platformResourceLoader';
 
 export default class Introduction extends LightningElement {
     MyPicture = Images + '/Abhishek_Smooth_fotor.jpg'
     github = Images + '/github2.png';
     leetcode = Images + '/leetcode2.png';
     linkedIn = Images + 'linkedin2.png';
-    gmail = Images + '/gmail2.png'
+    gmail = Images + '/gmail2.png';
+    // resume = RESUME +'/Abhishek_Aman_CV.pdf';
+
+    downloadResume() {
+        console.log(document.body);
+        // Create an invisible link and trigger the download
+        const link = document.createElement('a');
+        link.href = this.resume;
+        link.download = 'resume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 }
 
 // force-app\main\default\staticresources\Images\linkedin.png
